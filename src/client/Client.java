8d4c4f0ae06b8f4840bcc7759dc79a7db60edc78
Unknown <felix.sabatie@gmail.com>
2018-09-06 15:13:38 -0400

    Communication attempt between client and server

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,15 +1,48 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class Client {
 
   Socket socket;
+  InputStream in;
+  OutputStream out;
+  BufferedReader reader;
 
   public void run() {
     initSocket();
+    initStreams();
+    sendBasicRequest();
+  }
+
+  private void sendBasicRequest() {
+    System.out.println("Sending basic request...");
+    
+    try {
+      out.write("test".getBytes(Charset.forName("UTF-8")));
+      out.flush();
+    } catch (IOException e) {
+      System.out.println("Could not write to output stream");
+      closeConnection();
+      System.exit(3);
+    }
+
+    try {
+      String response = reader.readLine();
+      System.out.println("Message received from server :");
+      System.out.println(response);
+    } catch (IOException e) {
+      System.out.println("Could not read from input stream");
+      closeConnection();
+      System.exit(4);
+    }
   }
 
   private void initSocket() {
@@ -18,9 +51,23 @@ public class Client {
 
     try {
       socket = new Socket("127.0.0.1", serverPort);
-      System.out.println("Connection established to server");
+      System.out.println("Connection established to server\n");
     } catch (IOException e) {
       System.out.println("Could not connect to server on port " + serverPort);
+      closeConnection();
+      System.exit(1);
+    }
+  }
+
+  private void initStreams() {
+    try {
+      in = socket.getInputStream();
+      out = socket.getOutputStream();
+      reader = new BufferedReader(new InputStreamReader(in));
+    } catch (IOException e) {
+      System.out.println("Error during streams initialization");
+      closeConnection();
+      System.exit(2);
     }
   }
 
@@ -46,6 +93,18 @@ public class Client {
       keyboardScanner.nextLine();
     }
     return keyboardScanner.nextInt();
+  }
+
+  private void closeConnection() {
+    try {
+      reader.close();
+      in.close();
+      out.close();
+      socket.close();
+    } catch (IOException e) {
+      System.out.println("Error while closing connections");
+      System.exit(5);
+    }
   }
 
 }
--- src/server/Connection.java
+++ src/server/Connection.java
@@ -1,9 +1,18 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class Connection implements Runnable {
   Socket socket;
+  InputStream in;
+  OutputStream out;
+  BufferedReader reader;
 
   public Connection(Socket socket) {
     this.socket = socket;
@@ -11,6 +20,53 @@ public class Connection implements Runnable {
 
   @Override
   public void run() {
-    System.out.println("Executing...");
+    initStreams();
+    basicCommunication();
+  }
+
+  private void basicCommunication() {
+    System.out.println("Waiting for request...");
+    try {
+      String request = reader.readLine();
+      System.out.println("Message received from client :");
+      System.out.println(request);
+    } catch (IOException e) {
+      System.out.println("Could not read from input stream");
+      closeConnection();
+      System.exit(4);
+    }
+
+    try {
+      out.write("Hello client".getBytes(Charset.forName("UTF-8")));
+      out.flush();
+    } catch (IOException e) {
+      System.out.println("Could not write to output stream");
+      closeConnection();
+      System.exit(3);
+    }
+  }
+
+  private void initStreams() {
+    try {
+      in = socket.getInputStream();
+      out = socket.getOutputStream();
+      reader = new BufferedReader(new InputStreamReader(in));
+    } catch (IOException e) {
+      System.out.println("Error during streams initialization");
+      closeConnection();
+      System.exit(2);
+    }
+  }
+
+  private void closeConnection() {
+    try {
+      reader.close();
+      in.close();
+      out.close();
+      socket.close();
+    } catch (IOException e) {
+      System.out.println("Error while closing connections");
+      System.exit(5);
+    }
   }
 }
